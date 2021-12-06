90b91b5b417d500d7a7b755d1ae9e5f23f0f601c
joshhunt <josh@trtr.co>
2021-12-06 16:17:01 +0000

    delete files to force an update

--- bungie-website-output/site-source/Areas/User/AccountComponents/DataStores/ProfileDestinyMembershipDataStore.tsx
+++
@@ -1,8 +0,0 @@
-import { DestinyMembershipDataStore } from "@Global/DataStore/DestinyMembershipDataStore";
-
-class _ProfileDestinyMembershipDataStore extends DestinyMembershipDataStore {
-  public static Instance = new _ProfileDestinyMembershipDataStore();
-}
-
-export const ProfileDestinyMembershipDataStore =
-  _ProfileDestinyMembershipDataStore.Instance;
