5f8127ddb918b724c7db4bae0644ba911d98e47c
GitHub Action <action@github.com>
2022-06-09 17:56:40 +0000

    Site changed from automated check

--- bungie-website-output/site-source/Areas/Destiny/FreeToPlay/Components/FreeHero/FreeHero.tsx
+++ bungie-website-output/site-source/Areas/Destiny/FreeToPlay/Components/FreeHero/FreeHero.tsx
@@ -1,46 +1,101 @@
 // Created by a-bphillips, 2022
 // Copyright Bungie, Inc.
 
-import { FreeToPlayQuery } from "@Areas/Destiny/FreeToPlay/__generated__/FreeToPlayQuery.graphql";
-import {
-  FreeToPlayBuyBtn,
-  FreeToPlayResponsiveBg,
-} from "@Areas/Destiny/FreeToPlay/FreeToPlay";
+import { FreeToPlayBuyBtn } from "@Areas/Destiny/FreeToPlay/FreeToPlay";
 import { Responsive } from "@Boot/Responsive";
 import { useDataStore } from "@bungie/datastore/DataStoreHooks";
-import { imageFromConnection } from "@Utilities/GraphQLUtils";
+import { ImageVideoThumb } from "@UI/Marketing/ImageThumb";
+import {
+  bgImageFromStackFile,
+  responsiveBgImageFromStackFile,
+} from "@Utilities/GraphQLUtils";
 import React from "react";
+import { BnetStackFreeToPlayProductPage } from "../../../../../Generated/contentstack-types";
 import styles from "./FreeHero.module.scss";
 
 interface FreeHeroProps {
-  data: FreeToPlayQuery["response"]["free_to_play_product_page"]["hero"];
-  PlatformList: React.FC;
+  data: BnetStackFreeToPlayProductPage["hero"];
+  V2Data: BnetStackFreeToPlayProductPage["v2_hero"];
+  scrollToRewardsCallout: () => void;
 }
 
 export const FreeHero: React.FC<FreeHeroProps> = (props) => {
   const { mobile } = useDataStore(Responsive);
 
-  const { data, PlatformList } = props;
-  const { bg, btn_text, logoConnection } = data ?? {};
+  const { data, V2Data, scrollToRewardsCallout } = props;
+  const { bg, btn_text, logo } = data ?? {};
 
   return (
     <div
       className={styles.hero}
       style={{
-        backgroundImage: FreeToPlayResponsiveBg(
-          bg?.desktopConnection,
-          bg?.mobileConnection,
+        backgroundImage: responsiveBgImageFromStackFile(
+          undefined,
+          bg?.mobile,
           mobile
         ),
       }}
     >
+      {V2Data?.bg_video?.url && !mobile && (
+        <video
+          className={styles.bgVideo}
+          poster={bg?.desktop?.url}
+          loop
+          playsInline
+          autoPlay
+          muted
+        >
+          <source src={V2Data?.bg_video?.url} type={"video/mp4"} />
+        </video>
+      )}
+
+      <div className={styles.videoOverlay} />
+
       <div className={styles.contentWrapper}>
+        <img className={styles.logo} src={logo?.url} />
         <img
-          className={styles.logo}
-          src={imageFromConnection(logoConnection)?.url}
+          className={styles.heroesWelcomeLogo}
+          src={V2Data?.heroes_welcome_logo?.url}
         />
+        <p className={styles.subTitle}>{V2Data?.sub_heading}</p>
+
+        <div className={styles.trailerBtns}>
+          {V2Data?.trailer_btns?.map((btn, i) => {
+            return (
+              <div key={i} className={styles.trailerBtnWrapper}>
+                <ImageVideoThumb
+                  image={btn?.bg?.url}
+                  youtubeUrl={btn?.youtube_url}
+                  classes={{
+                    image: styles.bg,
+                    imageContainer: styles.trailerBtn,
+                  }}
+                />
+                <p className={styles.caption}>{btn?.caption}</p>
+              </div>
+            );
+          })}
+        </div>
+
         <FreeToPlayBuyBtn btn_text={btn_text} className={styles.buyBtn} />
-        <PlatformList />
+
+        <div className={styles.heroBug}>
+          <div
+            className={styles.bgImg}
+            style={{
+              backgroundImage: bgImageFromStackFile(
+                V2Data?.hero_bug?.background
+              ),
+            }}
+          />
+          <p className={styles.bugHeading} onClick={scrollToRewardsCallout}>
+            {V2Data?.hero_bug?.heading}
+          </p>
+          <span className={styles.arrows} onClick={scrollToRewardsCallout}>
+            <span className={styles.baseArrows} />
+            <span className={styles.animatedArrow} />
+          </span>
+        </div>
       </div>
     </div>
   );
--- bungie-website-output/site-source/Areas/Destiny/FreeToPlay/Components/ThreeGuardiansGraphic/ThreeGuardiansGraphic.tsx
+++ bungie-website-output/site-source/Areas/Destiny/FreeToPlay/Components/ThreeGuardiansGraphic/ThreeGuardiansGraphic.tsx
@@ -1,17 +1,14 @@
 // Created by a-bphillips, 2022
 // Copyright Bungie, Inc.
 
-import { FreeToPlayQueryResponse } from "@Areas/Destiny/FreeToPlay/__generated__/FreeToPlayQuery.graphql";
-import { FreeToPlayResponsiveBg } from "@Areas/Destiny/FreeToPlay/FreeToPlay";
 import { Responsive } from "@Boot/Responsive";
 import { useDataStore } from "@bungie/datastore/DataStoreHooks";
-import { Localizer } from "@bungie/localization/Localizer";
-import { imageFromConnection } from "@Utilities/GraphQLUtils";
 import React from "react";
+import { BnetStackFreeToPlayProductPage } from "../../../../../Generated/contentstack-types";
 import styles from "./ThreeGuardiansGraphic.module.scss";
 
 interface ThreeGuardiansGraphicProps {
-  guardians: FreeToPlayQueryResponse["free_to_play_product_page"]["guardians_section"]["guardians"];
+  guardians: BnetStackFreeToPlayProductPage["guardians_section"]["guardians"];
 }
 
 export const ThreeGuardiansGraphic: React.FC<ThreeGuardiansGraphicProps> = (
@@ -26,18 +23,11 @@ export const ThreeGuardiansGraphic: React.FC<ThreeGuardiansGraphicProps> = (
           <div key={i} className={styles.guardianWrapper}>
             <div className={styles.topContent}>
               <img
-                src={
-                  imageFromConnection(
-                    medium ? g.mobile_imageConnection : g.imageConnection
-                  )?.url
-                }
+                src={(medium ? g.mobile_image : g.image)?.url}
                 className={styles.guardian}
               />
               <div className={styles.titleWrapper}>
-                <img
-                  className={styles.logo}
-                  src={imageFromConnection(g.logoConnection)?.url}
-                />
+                <img className={styles.logo} src={g.logo?.url} />
                 <p className={styles.title}>{g.title}</p>
               </div>
             </div>
