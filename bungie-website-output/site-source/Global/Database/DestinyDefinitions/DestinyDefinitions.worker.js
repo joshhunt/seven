3a310e7f8a8583526a86383bdfbad5f3ec751c83
GitHub Action <action@github.com>
2026-01-13 17:22:27 +0000

    Site changed from automated check

--- bungie-website-output/site-source/Global/Database/DestinyDefinitions/DestinyDefinitions.worker.js
+++ bungie-website-output/site-source/Global/Database/DestinyDefinitions/DestinyDefinitions.worker.js
@@ -1,3 +1,3 @@
 module.exports = function () {
-  return new Worker(__webpack_public_path__ + "18d6e72412e52d6877c9.worker.js");
+  return new Worker(__webpack_public_path__ + "f6dc0b2cb7c01dce2c1f.worker.js");
 };
